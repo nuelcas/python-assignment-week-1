e17b360e2e5bc9f5b6f4e1bd0fed6727e37e76d5
Casmir Onyekani <casmir_ao@ymail.com>
2024-08-15 16:02:43 +0100

    Initial commit with user_input.py

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Enter user  name
+name = input("What is your name? ")
+
+# Enter user age
+age = input("How old are you? ")
+
+# Enter user location
+location = input("Where do you live? ")
+
+# Print out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
